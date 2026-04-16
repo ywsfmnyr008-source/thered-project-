0e06b323d2324ed287656e023267087242f01421
Yousef moneer <ywsfmnyr008@gmail.com>
2026-04-16 13:03:14 +0200

    thered project by myself

---
+++ main.js
@@ -0,0 +1,410 @@
+let countSpan = document.querySelector(".count span");
+let bulletsSpanContainer = document.querySelector(".bullets .spans");
+let currentIndex = 0;
+let rightAnswers = 0;
+let countdownInterval;
+let countdownElement = document.querySelector(".count-down");
+let quizArea = document.querySelector(".quiz-area");
+let answersArea = document.querySelector(".answers-area");
+let submitButton = document.querySelector(".submit-button");
+let resultsContainer = document.querySelector(".results");
+let bullets = document.querySelector(".bullets");
+function getQuestions() {
+    let myRequest = new XMLHttpRequest();
+
+    myRequest.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            let questionsObject = JSON.parse(this.responseText);
+            let questionCount = questionsObject.length;
+            createBullets(questionCount);
+
+            addQuestionData(questionsObject[currentIndex], questionCount);
+            countdown(5, questionCount);
+            submitButton.onclick = () => {
+                let theRightAnswer = questionsObject[currentIndex].right_answer;
+                console.log(theRightAnswer);
+                currentIndex++;
+                checkAnswer(theRightAnswer, questionCount);
+                quizArea.innerHTML = "";
+                answersArea.innerHTML = "";
+                addQuestionData(questionsObject[currentIndex], questionCount);
+
+                handleBullets();
+                clearInterval(countdownInterval);
+                countdown(5, questionCount);
+                showResults(questionCount);
+            }
+        }
+    };
+
+    myRequest.open("GET", "html_questions.json", true);
+    myRequest.send();
+}
+
+getQuestions();
+function createBullets(num) {
+    countSpan.innerHTML = num;
+    for (let i = 0; i < num; i++) {
+        let theBullet = document.createElement("span");
+        if (i === 0) {
+            theBullet.className = "on";
+        }
+        bulletsSpanContainer.appendChild(theBullet);
+    }
+}
+
+
+function addQuestionData(obj, count) {
+    if (currentIndex < count) {
+         let questionTitle = document.createElement("h2");
+    let questionText = document.createTextNode(obj["title"]);
+    questionTitle.appendChild(questionText);
+    quizArea.appendChild(questionTitle);
+    for (let i = 1; i <= 4; i++) {
+        let mainDiv = document.createElement("div");
+        mainDiv.className = "answer";
+        let radioInput = document.createElement("input");
+        radioInput.name = "question";
+        radioInput.type = "radio";
+        radioInput.id = `answer_${i}`;
+        radioInput.dataset.answer = obj[`answer_${i}`];
+        if (i === 1) {
+            radioInput.checked = true;
+        }
+        let theLabel = document.createElement("label");
+        theLabel.htmlFor = `answer_${i}`;
+        let theLabelText = document.createTextNode(obj[`answer_${i}`]);
+        theLabel.appendChild(theLabelText);
+        mainDiv.appendChild(radioInput);
+        mainDiv.appendChild(theLabel);
+        answersArea.appendChild(mainDiv);
+    }
+    }
+}
+
+function checkAnswer(rAnswer, count) {
+    let answers = document.getElementsByName("question");
+    let theChoosenAnswer;
+    for (let i = 0; i < answers.length; i++) {
+        if (answers[i].checked) {
+            theChoosenAnswer = answers[i].dataset.answer;
+        }
+    }
+    if (rAnswer === theChoosenAnswer) {
+        rightAnswers++;
+        console.log(`good`);
+    }
+}
+
+function handleBullets() {
+    let bulletsSpans = document.querySelectorAll(".bullets .spans span");
+    let arrayOfSpans = Array.from(bulletsSpans);
+    arrayOfSpans.forEach((span, index) => {
+        if (currentIndex === index) {
+            span.className = "on";
+        }
+    });
+}
+
+function showResults(count) {
+    let theResults;
+    if (currentIndex === count) {
+        quizArea.remove();
+        answersArea.remove();
+        submitButton.remove();
+        bullets.remove();
+        if (rightAnswers > count / 2 && rightAnswers < count) {
+            theResults = `<span class="good">Good</span>, ${rightAnswers} From ${count}`;
+        } else if (rightAnswers === count) {
+            theResults = `<span class="perfect">Perfect</span>, All Answers Is Good`;
+        } else {
+            theResults = `<span class="bad">Bad</span>, ${rightAnswers} From ${count}`;
+        }
+        resultsContainer.innerHTML = theResults;
+        resultsContainer.style.padding = "10px";
+        resultsContainer.style.backgroundColor = "white";
+        resultsContainer.style.marginTop = "10px";
+    }
+}
+function countdown(duration, count) {
+    if (currentIndex < count) {
+        let minutes, seconds;
+        countdownInterval = setInterval(function() {
+            minutes = parseInt(duration / 60);
+            seconds = parseInt(duration % 60);
+            minutes = minutes < 10 ? `0${minutes}` : minutes;
+            seconds = seconds < 10 ? `0${seconds}` : seconds;
+            countdownElement.innerHTML = `${minutes}:${seconds}`;
+            duration--;
+            if (duration < 0) {
+                clearInterval(countdownInterval);
+                submitButton.click();
+            }
+        }, 1000);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Select Elements
+// let countSpan = document.querySelector(".count span");
+// let bullets = document.querySelector(".bullets");
+// let bulletsSpanContainer = document.querySelector(".bullets .spans");
+// let quizArea = document.querySelector(".quiz-area");
+// let answersArea = document.querySelector(".answers-area");
+// let submitButton = document.querySelector(".submit-button");
+// let resultsContainer = document.querySelector(".results");
+// let countdownElement = document.querySelector(".countdown");
+
+// // Set Options
+// let currentIndex = 0;
+// let rightAnswers = 0;
+// let countdownInterval;
+
+// function getQuestions() {
+//   let myRequest = new XMLHttpRequest();
+
+//   myRequest.onreadystatechange = function () {
+//     if (this.readyState === 4 && this.status === 200) {
+//       let questionsObject = JSON.parse(this.responseText);
+//       let qCount = questionsObject.length;
+
+//       // Create Bullets + Set Questions Count
+//       createBullets(qCount);
+
+//       // Add Question Data
+//       addQuestionData(questionsObject[currentIndex], qCount);
+
+//       // Start CountDown
+//       countdown(3, qCount);
+
+//       // Click On Submit
+//       submitButton.onclick = () => {
+//         // Get Right Answer
+//         let theRightAnswer = questionsObject[currentIndex].right_answer;
+
+//         // Increase Index
+//         currentIndex++;
+
+//         // Check The Answer
+//         checkAnswer(theRightAnswer, qCount);
+
+//         // Remove Previous Question
+//         quizArea.innerHTML = "";
+//         answersArea.innerHTML = "";
+
+//         // Add Question Data
+//         addQuestionData(questionsObject[currentIndex], qCount);
+
+//         // Handle Bullets Class
+//         handleBullets();
+
+//         // Start CountDown
+//         clearInterval(countdownInterval);
+//         countdown(3, qCount);
+
+//         // Show Results
+//         showResults(qCount);
+//       };
+//     }
+//   };
+
+//   myRequest.open("GET", "html_questions.json", true);
+//   myRequest.send();
+// }
+
+// getQuestions();
+
+// function createBullets(num) {
+//   countSpan.innerHTML = num;
+
+//   // Create Spans
+//   for (let i = 0; i < num; i++) {
+//     // Create Bullet
+//     let theBullet = document.createElement("span");
+
+//     // Check If Its First Span
+//     if (i === 0) {
+//       theBullet.className = "on";
+//     }
+
+//     // Append Bullets To Main Bullet Container
+//     bulletsSpanContainer.appendChild(theBullet);
+//   }
+// }
+
+// function addQuestionData(obj, count) {
+//   if (currentIndex < count) {
+//     // Create H2 Question Title
+//     let questionTitle = document.createElement("h2");
+
+//     // Create Question Text
+//     let questionText = document.createTextNode(obj["title"]);
+
+//     // Append Text To H2
+//     questionTitle.appendChild(questionText);
+
+//     // Append The H2 To The Quiz Area
+//     quizArea.appendChild(questionTitle);
+
+//     // Create The Answers
+//     for (let i = 1; i <= 4; i++) {
+//       // Create Main Answer Div
+//       let mainDiv = document.createElement("div");
+
+//       // Add Class To Main Div
+//       mainDiv.className = "answer";
+
+//       // Create Radio Input
+//       let radioInput = document.createElement("input");
+
+//       // Add Type + Name + Id + Data-Attribute
+//       radioInput.name = "question";
+//       radioInput.type = "radio";
+//       radioInput.id = `answer_${i}`;
+//       radioInput.dataset.answer = obj[`answer_${i}`];
+
+//       // Make First Option Selected
+//       if (i === 1) {
+//         radioInput.checked = true;
+//       }
+
+//       // Create Label
+//       let theLabel = document.createElement("label");
+
+//       // Add For Attribute
+//       theLabel.htmlFor = `answer_${i}`;
+
+//       // Create Label Text
+//       let theLabelText = document.createTextNode(obj[`answer_${i}`]);
+
+//       // Add The Text To Label
+//       theLabel.appendChild(theLabelText);
+
+//       // Add Input + Label To Main Div
+//       mainDiv.appendChild(radioInput);
+//       mainDiv.appendChild(theLabel);
+
+//       // Append All Divs To Answers Area
+//       answersArea.appendChild(mainDiv);
+//     }
+//   }
+// }
+
+// function checkAnswer(rAnswer, count) {
+//   let answers = document.getElementsByName("question");
+//   let theChoosenAnswer;
+
+//   for (let i = 0; i < answers.length; i++) {
+//     if (answers[i].checked) {
+//       theChoosenAnswer = answers[i].dataset.answer;
+//     }
+//   }
+
+//   if (rAnswer === theChoosenAnswer) {
+//     rightAnswers++;
+//   }
+// }
+
+// function handleBullets() {
+//   let bulletsSpans = document.querySelectorAll(".bullets .spans span");
+//   let arrayOfSpans = Array.from(bulletsSpans);
+//   arrayOfSpans.forEach((span, index) => {
+//     if (currentIndex === index) {
+//       span.className = "on";
+//     }
+//   });
+// }
+
+// function showResults(count) {
+//   let theResults;
+//   if (currentIndex === count) {
+//     quizArea.remove();
+//     answersArea.remove();
+//     submitButton.remove();
+//     bullets.remove();
+
+//     if (rightAnswers > count / 2 && rightAnswers < count) {
+//       theResults = `<span class="good">Good</span>, ${rightAnswers} From ${count}`;
+//     } else if (rightAnswers === count) {
+//       theResults = `<span class="perfect">Perfect</span>, All Answers Is Good`;
+//     } else {
+//       theResults = `<span class="bad">Bad</span>, ${rightAnswers} From ${count}`;
+//     }
+
+//     resultsContainer.innerHTML = theResults;
+//     resultsContainer.style.padding = "10px";
+//     resultsContainer.style.backgroundColor = "white";
+//     resultsContainer.style.marginTop = "10px";
+//   }
+// }
+
+// function countdown(duration, count) {
+//   if (currentIndex < count) {
+//     let minutes, seconds;
+//     countdownInterval = setInterval(function () {
+//       minutes = parseInt(duration / 60);
+//       seconds = parseInt(duration % 60);
+
+//       minutes = minutes < 10 ? `0${minutes}` : minutes;
+//       seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+//       countdownElement.innerHTML = `${minutes}:${seconds}`;
+
+//       if (--duration < 0) {
+//         clearInterval(countdownInterval);
+//         submitButton.click();
+//       }
+//     }, 1000);
+//   }
+// }
